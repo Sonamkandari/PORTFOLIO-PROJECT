8bb56633bf5c850c8f5217f922830d972c6accc5
sonam kandari <sonamkandari2004@gmail.com>
2025-07-07 22:59:13 +0530

    Fix resume download and update styling

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,44 @@
 import React from 'react'
 // import the css file in this Hero component
-import'./Hero.css'
+import './Hero.css'
 import profile_img from '../../assets/profile.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 function Hero() {
   return (
-    <div id='home'className='hero'>
-        {/* add profile image here */}
-        <img src={profile_img} alt=''/>
-        <h1><span>I'm Sonam Kandari,</span> Software Engineer based in India</h1>
-        <p>I am a Computer Science undergraduate with a strong focus on web development, Java programming, and problem-solving.</p>
-        <div className="hero-action">
-            {/* here we will create two action buttons */}
-            <div className="hero-connect"><AnchorLink className='anchor-link'offset={50}href='#contact'>Connect with me</AnchorLink></div>
-                 <a 
-                 className="hero-resume"
-                  href="/Resume.pdf"
-                  download="Resume.pdf"
-                > Download my resume</a>
+    <div id='home' className='hero'>
+      {/* add profile image here */}
+      <img src={profile_img} alt='' />
+      
+      {/* main heading with name and title */}
+      <h1><span>I'm Sonam Kandari,</span> Software Engineer based in India</h1>
+      
+      {/* brief description */}
+      <p>
+        I am a Computer Science undergraduate with a strong focus on web development, Java programming, and problem-solving.
+      </p>
+      
+      <div className='hero-action'>
+        {/* here we will create two action buttons */}
+        <div className='hero-connect'>
+          <AnchorLink
+            className='anchor-link'
+            offset={50}
+            href='#contact'
+          >
+            Connect with me
+          </AnchorLink>
         </div>
+
+        {/* download resume link styled as button */}
+        <a
+          className='hero-resume'
+          href='/Resume.pdf'
+          download
+        >
+          Download my resume
+        </a>
+      </div>
     </div>
   )
 }
